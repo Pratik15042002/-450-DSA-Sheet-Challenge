60ed39d2163b6a4cadf13a3a661e14e2d02d6dfe
Pratik Khandare <107782515+Pratik15042002@users.noreply.github.com>
2022-12-28 23:12:12 +0530

    Update 03_kth_max_min.cpp

--- 03_kth_max_min.cpp
+++ 03_kth_max_min.cpp
@@ -1,10 +1,44 @@
-/*
-    link: https://practice.geeksforgeeks.org/problems/kth-smallest-element5635/1
-
-    sol: https://www.geeksforgeeks.org/kth-smallestlargest-element-unsorted-array/
-    or
-    refer heap/5th_kth....cpp
-
-*/
-
-
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int kthLargest(int stream[], int n, int k)
+{
+   // Create a min heap and store first k-1 elements
+   // of stream into
+   priority_queue<int, vector<int>, greater<int> > pq;
+ 
+   // Push first k elements and print "_" (k-1) times
+   for (int i=0; i<k-1; i++)
+   {
+      pq.push(stream[i]);
+      cout << "_ ";
+   }
+   pq.push(stream[k-1]);
+ 
+   for (int i=k; i<n; i++)
+   {
+       // We must insert last element before we
+       // decide last k-th largest output.
+          cout << pq.top() << " ";
+ 
+       if (stream[i] > pq.top())
+       {
+           pq.pop();
+           pq.push(stream[i]);
+       } 
+   } 
+ 
+   // Print last k-th largest element (after
+   // (inserting last element)
+   cout << pq.top();
+}
+ 
+// Driver code
+int main()
+{
+   int arr[] = {10, 20, 11, 70, 50, 40, 100, 55};
+   int k = 3;
+   int n = sizeof(arr)/sizeof(arr[0]);
+   kthLargest(arr, n, k);
+   return 0;
+}
