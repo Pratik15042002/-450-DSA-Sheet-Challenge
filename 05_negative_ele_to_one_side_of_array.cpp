62d296987dddd21b5f52e8c02019df18ae5b8949
Pratik Khandare <107782515+Pratik15042002@users.noreply.github.com>
2022-12-30 19:51:55 +0530

    Update 05_negative_ele_to_one_side_of_array.cpp

--- 05_negative_ele_to_one_side_of_array.cpp
+++ 05_negative_ele_to_one_side_of_array.cpp
@@ -1,11 +1,4 @@
-/*
-    link: https://www.geeksforgeeks.org/move-negative-numbers-beginning-positive-end-constant-extra-space/
 
-    variation: 2-pointer
-*/
-
-
-// ----------------------------------------------------------------------------------------------------------------------- //
 void arrange(int a[], int n) {
     int negi = 0;
     int posi = n - 1;
