aa70131d78b15c33c32578abee3b1cd2fd652b50
Pratik Khandare <107782515+Pratik15042002@users.noreply.github.com>
2022-12-31 18:33:41 +0530

    Add files via upload

---
+++ reverse aray -own.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> 
+void reverseArray(vector<int> &arr , int m)
+{
+	int s =m+1  ;
+    int e= arr.size()-1;
+  
+    for(int i= s; i<=e;i++){
+        swap(arr[s] , arr[e]);
+        s++;
+        e--;;  
+       
+    }
+
+}
