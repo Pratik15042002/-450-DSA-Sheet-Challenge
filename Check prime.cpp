a04204fbb8664f756dc4a493503b4799af36cbb4
Pratik Khandare <107782515+Pratik15042002@users.noreply.github.com>
2023-06-15 11:32:35 +0530

    Add files via upload

---
+++ Check prime.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    for( int i=2;i<n;i++){
+    if(n%i==0){
+   cout<<"false ";
+  
+    }
+    else {
+    cout<<" true ";
+  
+    }
+     break;
+    }
+    return 0;
+}
