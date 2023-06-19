a4a0b17b4d95527147597199e99c51f3900f9375
Pratik Khandare <107782515+Pratik15042002@users.noreply.github.com>
2023-06-19 10:15:42 +0530

    Add files via upload

---
+++ MDA vector of vector.cpp
@@ -0,0 +1,31 @@
+ 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int
+main ()
+{
+   int m=3;
+   int n=2;
+  vector < vector < int >>arr;
+  for (int i = 0; i < m; i++)
+    {
+      vector < int >v;
+      for (int j = 0; j < n; j++)
+	{
+	  v.push_back (1234);
+	  arr.push_back (v);
+
+	}
+      for (int i = 0; i < arr.size(); i++)
+	
+	  for (int j = 0; j < arr[i].size(); j++)
+	    {
+	      cout << arr[i][j];
+	    }
+	    cout<<endl;
+	}
+    
+  return 0;
+}
